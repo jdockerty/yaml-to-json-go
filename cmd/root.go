c14c6197a691bcb1000127f3a5cafb8c0027b0d1
Jack Dockerty <jdockerty19@gmail.com>
2020-11-01 14:55:27 +0000

    Adding root command using Cobra.

--- cmd/root.go
+++ cmd/root.go
@@ -43,11 +43,12 @@ to quickly create a Cobra application.`,
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
+func Execute() int {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
-		os.Exit(1)
+		return 1
 	}
+	return 0
 }
 
 func init() {
