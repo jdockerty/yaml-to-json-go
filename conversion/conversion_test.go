8d03fe3946d4e7958c91000f7612f8f57f55cde2
Jack Dockerty <jdockerty19@gmail.com>
2020-11-01 18:48:10 +0000

    Updating tests for JSON and capitalising YAML for consistency.

--- conversion/conversion_test.go
+++ conversion/conversion_test.go
@@ -6,6 +6,7 @@ import (
 )
 
 var localYAMLFile string
+var localJSONFile string
 
 func init() {
 	localYAMLFile = "../test-files/test.yml"
@@ -17,9 +18,23 @@ func init() {
 	// 	parents:
 	// 		- Sally
 	// 		- Robert
+
+	localJSONFile = "../test-files/test.json"
+	// This file contains
+	//
+	// {
+	// 	"age": 29,
+	// 	"name": "Jimmy",
+	// 	"relationships": {
+	// 		"parents": [
+	// 			"Sally",
+	// 			"Robert"
+	// 		]
+	// 	}
+	// }
 }
 
-func TestCanReadUnstructuredYamlFile(t *testing.T) {
+func TestCanReadUnstructuredYAMLFile(t *testing.T) {
 
 	assert := assert.New(t)
 
@@ -37,7 +52,24 @@ func TestCanReadUnstructuredYamlFile(t *testing.T) {
 	assert.Equal(yamlData, expectedYaml)
 }
 
-func TestOutputYamlIsCorrect(t *testing.T) {
+func TestCanReadUnstructuredJSONFile(t *testing.T) {
+
+	assert := assert.New(t)
+
+	jsonData, err := UnmarshalJSONFile(localJSONFile)
+	assert.Nil(err)
+
+	expectedJSON := map[string]interface{}{
+		"age": float64(29),
+		"name": "Jimmy",
+		"relationships": map[string]interface{}{
+			"parents": []interface{}{"Sally", "Robert"},
+		},
+	}
+	assert.Equal(jsonData, expectedJSON)
+}
+
+func TestOutputYAMLIsCorrect(t *testing.T) {
 
 	assert := assert.New(t)
 
@@ -56,7 +88,26 @@ func TestOutputYamlIsCorrect(t *testing.T) {
 
 }
 
-func TestFullConversionIsCorrect(t *testing.T) {
+func TestOutputJSONIsCorrect(t *testing.T) {
+	assert := assert.New(t)
+
+	jsonData, err := UnmarshalJSONFile(localJSONFile)
+	assert.Nil(err)
+
+	incorrectJSON := map[string]interface{}{
+		"age": float64(30),
+		"name": "Ricky",
+		"relationships": map[string]interface{}{
+			"parents": []interface{}{"Mike", "Robert"},
+		},
+	}
+
+	assert.NotEqual(jsonData, incorrectJSON)
+}
+
+
+
+func TestFullYAMLConversionIsCorrect(t *testing.T) {
 	assert := assert.New(t)
 
 	jsonData, err := YAMLToJSONFull(localYAMLFile)
@@ -66,7 +117,7 @@ func TestFullConversionIsCorrect(t *testing.T) {
 	assert.IsType(expectedType, jsonData)
 }
 
-func TestConvertYamlToJSON(t *testing.T) {
+func TestConvertYAMLToJSON(t *testing.T) {
 	assert := assert.New(t)
 
 	yamlData, err := UnmarshalYAMLFile(localYAMLFile)
@@ -75,6 +126,9 @@ func TestConvertYamlToJSON(t *testing.T) {
 	jsonOutput, err := YAMLToJSON(yamlData)
 	assert.Nil(err)
 
+	var expectedType []byte
+	assert.IsType(expectedType, jsonOutput)
+	
 	t.Logf("%s\n", jsonOutput)
 	// Output:{
 	// "age": 29,
@@ -86,4 +140,16 @@ func TestConvertYamlToJSON(t *testing.T) {
 	//		]
 	// 	}
 	// }
+}
+
+
+func TestConvertJSONToYAML(t *testing.T) {
+	assert := assert.New(t)
+
+	jsonData, _ := UnmarshalJSONFile(localJSONFile)
+
+	yamlOutput, err := JSONToYAML(jsonData)
+	assert.Nil(err)
+
+	t.Logf("\n%s", yamlOutput)
 }
